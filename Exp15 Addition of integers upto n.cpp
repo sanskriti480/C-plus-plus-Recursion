422d98b2c30280afe55a9ad12d38d7be2c6ea519
sanskriti480 <sanskriti.ten@gmail.com>
2025-09-14 14:46:36 +0530

    Create Exp15 Addition of integers upto n.cpp

---
+++ Exp15 Addition of integers upto n.cpp
@@ -0,0 +1,40 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 14
+
+#include <iostream>
+using namespace std;
+
+int sumup(int n) {
+    if (n < 0) {
+        cout << "Error: Sum is not defined for negative numbers." << endl;
+        return 0;  // return 0 for invalid input
+    }
+    if (n <= 1)
+        return 1;
+    else
+        return (n + sumup(n - 1));  // recursive call
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int result = sumup(num);
+    if (num >= 0) {
+        cout << "Sum of numbers from 1 to " << num << " is " << result << endl;
+    }
+
+    return 0;
+}
+
+/*
+Output: 
+Enter a number: 6
+Sum of numbers from 1 to 6 is 21
+
+Enter a number: -3
+Error: Sum is not defined for negative numbers.
+*/
